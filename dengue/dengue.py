cf5feecf8a61f2093a6a18735a2eca09f59a5e48
factori7 <danilopujaico@gmail.com>
2024-09-12 00:14:54 -0500

    Update dengue.py

--- dengue/dengue.py
+++ dengue/dengue.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask,render_template
 
 app = Flask("my-dengue")
@@ -8,4 +9,4 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000)) 
-    app.run(host='0.0.0.0', port=port)   
+    app.run(host='0.0.0.0', port=port)
